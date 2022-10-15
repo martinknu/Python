b17273ddc3d7bdb7ffcfcc7c95d71a67a532a588
martinknu <martinknu@gmail.com>
2022-10-16 00:55:01 +0300

    fixed createdivimages

--- Python/htmlgenerators/createdivimgs/createdivimgs.py
+++ Python/htmlgenerators/createdivimgs/createdivimgs.py
@@ -3,45 +3,42 @@
 #Purpose: To list all jpg files in a folder and create HTML code with the images.
 #V001.000: First version
 
+import os
 from copy import copy
 from os import walk
 
 
 imgFolder = "/home/martink/sites/python/Python/htmlgenerators/createdivimgs/images"
+wwwFolder = "media/"
 imgType = ""
 filesList = []
-imgDict = {"name": "name", "path": "path","desc": "description", "dirpath": "dirpath", "dirnames": "dirnames"}
+imgDict = {"name": "", "path": "","size": ""} 
 selList = []
 htmlOutput = "htmlpics.txt"
 
 
 # Walk folder and find all files
 for (dirpath, dirnames, filenames) in walk(imgFolder):
-    imgDict["name"] = filenames
-    imgDict["path"] = dirpath
-    imgDict["dirnames"] = dirnames
+    filesList.extend(filenames)
 
-    print(imgDict)
-    filesList.extend(imgDict)
+# Filter files for specific type and get additional info
+for x in filesList:
+    if x[-len(imgType):] == imgType or imgType == "":
+        fileInfo = os.stat(imgFolder + "/" + x)
+        imgDict["name"] = x
+        imgDict["path"] = imgFolder + "/" + x
+        imgDict["size"] = fileInfo.st_size
 
-# Filter files for specific type
-if imgType == "":
-    selList = copy(filesList)
-else:
-    for x in filesList: 
-        if x["name"][-len(imgType):] == imgType:
-            #print(x)
-            selList.append(x)
+        imgDictCopy = imgDict.copy()
+        selList.append(imgDictCopy)
 
 
-#print(selList)
-
-"""
 # Create file with images
 with open(htmlOutput , "w") as file:
 
     for x in selList:
-        strHTML = "<img style=\"background-color: " + x + " \"> " + x +  " </div>"
+        strHTML = "<img src=\"" + wwwFolder + x["name"] + "\" alt=\"" + x["name"] + "\" width=\"500\"  style=\"display: block; margin:10px; border-radius: 15px\" >"
         file.write(f'{strHTML}\n')
 
-        """
+
+# height=\"600\"
