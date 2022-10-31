a975a4cb372eb7d639a05d6126a85115cc6f344c
martinknu <martinknu@gmail.com>
2022-10-31 23:19:29 +0100

    added createsinsvg to create curvy svg's

---
+++ Python/svggenerators/createsinsvg.py
@@ -0,0 +1,49 @@
+#Created: 2022-10-14
+#Author: Martin Knudsen
+#Purpose: To create svg from math expression sinodial curve
+#V001.000: First version
+
+import ctypes
+import math 
+
+
+# Messagebox
+def Mbox(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+
+
+
+points = 100
+amplitude = 5.0
+svgHeight = 100
+svgWidth = 100
+svgOutput = "sinodialpic.svg"
+shape = []
+
+
+# Create shape
+i = 0
+while i < points:
+    #print(i)
+    #print(math.sin(i))
+    shape.append([i,  math.sin(math.radians(360)/points*i)])
+    i += 1
+
+
+
+# Create svg
+with open(svgOutput , "w") as file:
+    
+    file.write(f'<svg height=\"{svgHeight}\" width=\"{svgWidth}\">\n')
+    file.write('<polyline points=\"')
+    for x in shape:
+        print(x)
+        file.write(str(x[0]) + "," + str((svgHeight/2) + (x[1])* amplitude) + " ")
+    
+    file.write(f'{svgWidth},{svgHeight/2} ')
+    file.write(f'{svgWidth},{svgHeight} ')
+    file.write(f'0,{svgHeight} ')
+    #file.write(f'0,{svgHeight/2}')
+    file.write('\"\n')
+    file.write('fill="black" stroke="black" />')
+    file.write('</svg>')
