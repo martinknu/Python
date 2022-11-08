d15af733090a36d8e064a2dd73c1657df2ea7496
martinknu <martinknu@gmail.com>
2022-11-08 13:24:32 +0100

    editing createrandombeizerstone

--- Python/svggenerators/createrandombeizerstone.py
+++ Python/svggenerators/createrandombeizerstone.py
@@ -12,49 +12,76 @@ def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
 xmin = 5
-xmax = 95
+xmax = 30
 ymin = 5
-ymax = 95
+ymax = 30
 dist = 50
 xcord = 0
 ycord = 0
 svgHeight = 100
 svgWidth = 100
 svgOutput = "stone.svg"
-shape = []
+shape = [[],[]]
 
 
 # Create shape coordinates
 
-xcord = random.randint(xmin, xmax)
-ycord = random.randint(ymin, ymax)
+xcord = 20#random.randint(xmin, xmax)
+ycord = 50 #random.randint(ymin, ymax)
+
+print(f'x, ycord: {xcord} , {ycord}')
+
+xadd = 20#random.randint(xmin, xmax )
+yadd = 20#random.randint(ymin, ymax )
+
+print(f'x, y add: {xadd} , {yadd}')
+
+#left
+
+shape[0].append([xcord, ycord]) 
+shape[0].append([xcord+xadd, ycord+yadd]) 
+
+#right
+shape[0].append([xcord+dist, ycord])
+shape[0].append([xcord+dist-xadd, ycord+yadd]) 
+
+#top left 
+shape[1].append([xcord, ycord]) 
+shape[1].append([xcord+xadd, ycord-yadd]) 
+
+#top right
+shape[1].append([xcord+dist, ycord-yadd])
+shape[1].append([xcord+dist-xadd, ycord-yadd]) 
+
+print(shape)
 
 
-shape.append([xcord, ycord]) 
-
-xadd = random.randint(-xmin, xmin)
-yadd = random.randint(-ymin, ymin)
-shape.append([xcord+xadd, ycord+yadd]) 
-
-
-xadd = random.randint(-xmin, xmin)
-yadd = random.randint(-ymin, ymin)
-shape.append([xcord+xadd, ycord+yadd]) 
-
-
+exit
 # Create svg file with coordinates
 with open(svgOutput , "w") as file:
     
     file.write(f'<svg height=\"{svgHeight}\" width=\"{svgWidth}\" \n')
     file.write('xmlns=\"http://www.w3.org/2000/svg\">\n')
-    file.write(f'<path d="M {xcord} {ycord} C ')
+
     for i , x in enumerate(shape):
-        if i != len(shape) - 1:
-            file.write(str(x[0]) + " " + str(x[1])  + ", ")
-        else:
-            file.write(str(x[0]) + " " + str(x[1]))           
-    file.write('\"\n')
-    file.write('fill="black" stroke="black" />')
+        print(f"X: {x}")
+        for z , q in enumerate(x):
+            print(z)
+            if z == 0:
+                file.write(f'<path d="M {xcord} {ycord} C ')
+
+            if z != len(q) :
+                file.write(str(q[0]) + " " + str(q[1])  + ", ")
+            else:
+                file.write(str(q[0]) + " " + str(q[1]))
+
+            if z == len(q):
+                file.write('\" ')
+                file.write('fill="none" stroke="black" />\n')
+
+        #else:
+            #file.write(str(x[0]) + " " + str(x[1]))           
+
     file.write('</svg>')
 
 print("Done")
