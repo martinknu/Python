83d42b62c8f8498a7c10c83061a6f1570aff9fdd
martinknu <martinknu@gmail.com>
2022-11-20 22:49:55 +0100

    editing pypdf2write, trying to center text

--- Python/pypdf2writepage/pypdfwrite.py
+++ Python/pypdf2writepage/pypdfwrite.py
@@ -18,6 +18,7 @@ def Mbox(title, text, style):
 
 #Declerations
 mypage = PageObject().create_blank_page(pdf=None, width=1680,height=1050)
+mystr = "String object"
 print(mypage)
 
 
@@ -28,16 +29,33 @@ output = open("breakerPage.pdf", "wb")
 writer.add_page(mypage)
 # Add the annotation
 annotation = AnnotationBuilder.free_text(
-    "Automatic inserted annotation\nThis is the second line!",
-    rect=(0, 0, 1680, 1050),
-    font="Arial",
+    text="Automatic inserted annotation\nThis is the second line!",
+    rect=(500, 550, 1180, 1000),
+    font="Helvetica",
     bold=True,
     italic=False,
-    font_size="3pt",
+    font_size="30pt",
     font_color="000000",
     border_color="000000",
-    background_color="FFFFFF",
+    background_color="C2C2C2",
 )
+#print(annotation)
+if "text-align" in annotation:
+    print("the key is there")
+
+
+#annotation["text-align"] = "center"
+print(f'Annotation object: {annotation}')
+
+print(f'Annotation keys: {annotation.keys()}')
+
+print(f'Examine typeof: {type(annotation)}')
+
+#annotation["/DS"] = "font: bold Helvetica 14pt;text-align:center;color:#000000"
+print(annotation["/DS"])
+print(type(annotation["/DS"]))
+
+
 writer.add_annotation(page_number=0, annotation=annotation)
 
 print(f'Pages: {writer.getNumPages()}')
