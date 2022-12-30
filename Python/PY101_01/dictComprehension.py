35e657f9b321ee503b6e45c50529593b953c3a62
martinknu <martinknu@gmail.com>
2022-12-31 00:13:53 +0200

    changes to dictcomprehension

--- Python/PY101_01/dictComprehension.py
+++ Python/PY101_01/dictComprehension.py
@@ -1,19 +1,23 @@
+#Dictionaries and dictcomprehension
+#
 
 
-
-
+#This is a list containing 2 touples
 names = [("name","Martin"), ("Occupation","Full stack")]
+print(f'type of name: {type(names)}')
+print(f'type of name index 0: {type(names[0])}')
 
 # Create a dictionary the long way
 d = {}
 for key, value in names:
     d[key] = value
 print(d)
+print(f'the type is:{type(d)}')
 
 #Create with dictoinary comprehension
 d = {key: value for key, value in names}
 print(d)
 
-#Create whit function :-)
+#Create with dict constructor
 d = dict(names)
 print(d)
