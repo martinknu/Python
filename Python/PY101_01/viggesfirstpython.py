6d3840270d212f9dfe8a4e906bcdd2ee0184d43a
martinknu <martinknu@gmail.com>
2023-01-02 18:35:57 +0200

    Victors first program added

---
+++ Python/PY101_01/viggesfirstpython.py
@@ -0,0 +1,10 @@
+
+
+
+#for i in range(1000000):
+#    print(f'Victor is awesome! {i} times.')
+while True:
+    myadd001 = input("Give me the first number ")
+    myadd002 = input("Give me the second number ")
+
+    print(f'The answer is: {int(myadd001) + int(myadd002)}')
